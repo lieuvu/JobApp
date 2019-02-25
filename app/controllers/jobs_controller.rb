46d07f802ddae62a625e96b1c2de967dff839438
Lieu Vu <dinhlieuvu@gmail.com>
2019-02-25 23:28:18 +0200

    Modify jobs controller and its related views

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,25 +1,61 @@
 class JobsController < ApplicationController
+  
+  # ---------------
+  # Include helpers
+  # ---------------
+
   include SessionsHelper
+
+  # --------------
+  # Filter methods
+  # --------------
+
   before_action :set_job, only:[:show]
-  before_action :check_logged_in?, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_logged_in?,
+    only: [:new, :create, :edit, :update, :destroy]
+
+  # ------
+  # Layout
+  # ------
+
+  layout 'main_login'
   
   
   #GET /jobs
   #GEt /jobs.json
   def index
-    if current_user && params[:user_id] 
-      @jobs = Job.where(user_id: current_user.id)
-      render 'jobs/jobs_posted'
-    elsif params[:sort_by].nil?
-      @jobs = Job.all
-    elsif
-      @jobs = Job.sort(params[:sort_by])
-    end
+    # If a user is logged in
+    if current_user then
+      # If has user id
+      if params[:user_id] then
+        @jobs = Job.where(user_id: current_user.id)
+        render 'jobs_posted' 
+      end
+
+      # Find jobs with params sort by
+      if params[:sort_by].present?
+        @jobs = Job.sort(params[:sort_by])
+        return
+      else
+        # Otherwise, find all jobs
+        @jobs = Job.all
+      end
+    else
+      # Otherwise, the user is not logged in
+
+      if params[:sort_by].present? then
+        @jobs = Job.sort(params[:sort_by])
+      else
+        @jobs = Job.all
+      end
+
+      render layout: 'main_non_login'
+    end # end if current user
   end
   
   # GET /users/:user_id/jobs/new
   def new
-    if !current_user.nil? && current_user.role.downcase == "company"
+    if !current_user.nil? && current_user.role.downcase == 'company'
       @job = Job.new
     else
       redirect_to root_path
@@ -41,7 +77,7 @@ class JobsController < ApplicationController
       if @job.save
         format.html do
           redirect_to job_path(@job.id),
-                      notice: "Job was successfully created"
+                      notice: 'Job was successfully created'
         end
         format.json { render action: 'show', status: :created, location: @job }
       else
@@ -54,7 +90,7 @@ class JobsController < ApplicationController
   # GET /users/:user_id/jobs/1/edit
   def edit
     @job = Job.find(params[:id])
-    @job[:date_valid] = @job.date_valid.strftime("%d.%m.%Y")
+    @job[:date_valid] = @job.date_valid.strftime('%d.%m.%Y')
     return @job
   end
 
@@ -65,7 +101,7 @@ class JobsController < ApplicationController
 
     respond_to do |format|
       if @job.update(test_params)
-        format.html {redirect_to @job, notice: "Job was successfully updated"}
+        format.html {redirect_to @job, notice: 'Job was successfully updated'}
         format.json {render action: 'show', location: @job}
       else
         format.html {render action: 'edit'}
@@ -81,22 +117,20 @@ class JobsController < ApplicationController
     redirect_to user_jobs_path
   end
 
-  private
+  # ---------------
+  # Private methods
+  # ---------------
 
-    def set_job
-      if !Job.exists?(params[:id])
-        redirect_to root_path
-      else
-        @job = Job.find(params[:id])
-      end
-    end
+  private def set_job
+    redirect_to root_path if !Job.exists?(params[:id])
+  end
 
-    def test_params
-      params[:job][:user_id] = params[:user_id]
-      params.require(:job).permit(:user_id, :job_title, :job_des, :date_valid)
-    end
+  private def test_params
+    params[:job][:user_id] = params[:user_id]
+    params.require(:job).permit(:user_id, :job_title, :job_des, :date_valid)
+  end
 
-    def check_logged_in?
-      redirect_to root_path if current_user.blank?
-     end
+  private def check_logged_in?
+    redirect_to root_path if current_user.blank?
+  end
 end
