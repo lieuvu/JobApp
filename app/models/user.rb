6631a9e7488a1d22dede49aba85e5b3fd1ee703c
Lieu Vu <dinhlieuvu@gmail.com>
2019-02-26 22:49:07 +0200

    Improve user model logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ActiveRecord::Base
+	
+	# -------
+	# Include
+	# -------
+	
 	include SecureRandom
+	
+	# ------------
+	# Relationship
+	# ------------
+
 	has_many :jobs, dependent: :destroy
 
+	# --------------
+	# Filter methods
+	# --------------
 
-	before_save(:on => :create) do
+	before_create :assign_access_code
+	
+	private def assign_access_code
 		begin
 			temp = SecureRandom.uuid()
 		end while User.where(access_code: temp).present?
